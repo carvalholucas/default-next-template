4e374ab677d8a5742583aec49ba0b28fa24c067c
Lucas Carvalho <carvalho.lucassm@hotmail.com>
2022-08-05 18:09:34 -0300

    Fix typography component

--- components/Typography/index.js
+++ components/Typography/index.js
@@ -1,10 +1,29 @@
 import { Container } from './styles.js'
 
-const Typography = ({ variant, color, children, ...props }) => {
+const variantsMapping = {
+  h1: "h1",
+  h2: "h2",
+  h3: "h3",
+  h4: "h4",
+  h5: "h5",
+  h6: "h6",
+  subheading1: "h6",
+  subheading2: "h6",
+  body1: "p",
+  body2: "p",
+};
+
+const Typography = ({ variant, color, mb, children, ...props }) => {
+
+  const Component = variant ? variantsMapping[variant] : "p";
+  const marginBottom = mb?.toString().concat('px')
+
   return (
     <Container
-      as={variant}
+      as={Component}
+      color={color}
       variant={variant}
+      mb={marginBottom}
       {...props}
     >
       {children}
--- components/Typography/styles.js
+++ components/Typography/styles.js
@@ -19,42 +19,42 @@ const typographyModifiers = {
   h6: (theme) => css`
     font-size: ${theme.font.sizes.xxsmall};
   `,
-  subheading1: (theme) => css`
+  subheading1: (theme, mb) => css`
     font-size: ${theme.font.sizes.medium};
     color: #6d7f82;
     line-height: ${theme.font.lineHeights.body};
-    margin-bottom: ${theme.spacings.xsmall};
+    margin-bottom: ${mb ? mb : theme.spacings.xsmall};
   `,
-  subheading2: (theme) => css`
+  subheading2: (theme, mb) => css`
     font-size: ${theme.font.sizes.small};
     color: #6d7f82;
     line-height: ${theme.font.lineHeights.body};
-    margin-bottom: ${theme.spacings.xsmall};
+    margin-bottom: ${mb ? mb : theme.spacings.xsmall};
   `,
-  body1: (theme) => css`
+  body1: (theme, mb) => css`
     font-size: ${theme.font.sizes.xsmall};
     font-weight: ${theme.font.fontWeights.body};
     color: #465b6f;
     line-height: ${theme.font.lineHeights.body};
-    margin-bottom: ${({ theme }) => theme.spacings.medium};
+    margin-bottom: ${mb ? mb : theme.spacings.medium};
   `,
-  body2: (theme) => css`
+  body2: (theme, mb) => css`
     font-size: ${theme.font.sizes.xxsmall};
     font-weight: ${theme.font.fontWeights.body};
     color: #465b6f;
     line-height: ${theme.font.lineHeights.body};
-    margin-bottom: ${({ theme }) => theme.spacings.medium};
+    margin-bottom: ${mb ? mb : theme.spacings.medium};
   `,
 }
 
 export const Container = styled.p`
-  ${({ theme, variant, color }) => css`
+  ${({ theme, variant, color, mb }) => css`
     color: ${color ? color : '#2A2A2A'};
     display: block;
     font-weight: ${theme.font.fontWeights.heading};
     line-height: ${theme.font.lineHeights.body};
-    margin-bottom: .5em;
+    margin-bottom: ${mb ? mb : '.5em'};
 
-    ${variant && typographyModifiers[variant](theme)}
+    ${variant && typographyModifiers[variant](theme, mb)}
   `}
 `
