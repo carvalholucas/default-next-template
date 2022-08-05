0640570967df735d0c3c1cadb85ed13bcb71b59b
Lucas Carvalho <carvalho.lucassm@hotmail.com>
2022-08-05 01:03:02 -0300

    Change section component

--- components/Section/styles.js
+++ components/Section/styles.js
@@ -1,5 +1,4 @@
 import styled, { css } from 'styled-components'
-import media from "styled-media-query"
 
 export const SectionWrapper = styled.section`
   ${({ theme }) => css`
@@ -9,9 +8,5 @@ export const SectionWrapper = styled.section`
     & > * {
       height: 100%;
     }
-
-    /* ${media.greaterThan("medium")`
-
-    `} */
   `}
 `
